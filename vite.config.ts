c5b0e560c823cda0ea83f5c98eebad4eb5a5df01
Daria <zhezherya_d@mail.ru>
2024-04-24 01:23:05 +0300

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
 import {  defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv';
-
 dotenv.config();
-
 const { VITE_AUTH_TOKEN} = process.env;
 
 export default defineConfig({
     server: {
-        port: 3000,
         proxy: {
             '/api':{
                 target: 'https://localhost:3000', 
